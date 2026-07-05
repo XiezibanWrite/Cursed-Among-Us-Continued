5aaac5e21e45ddcbd32939f856472ade8070355f
linmeideli <2558527373@qq.com>
2026-07-05 19:10:40 +0800

    Fix

--- CursedAmongUs/Source/CredentialsPatch.cs
+++ CursedAmongUs/Source/CredentialsPatch.cs
@@ -13,59 +13,67 @@ namespace CursedAmongUs.Source
 	[HarmonyPatch]
 	public class CredentialsPatch
 	{
+		public static bool _allowShowModInfos = true;
+
 		[HarmonyPatch(typeof(PingTracker), nameof(PingTracker.Update))]
 		public static class PingTrackerPatch
 		{
 			static void Postfix(PingTracker __instance)
 			{
-				__instance.text.alignment = TextAlignmentOptions.Top;
-				var position = __instance.GetComponent<AspectPosition>();
-				position.Alignment = AspectPosition.EdgeAlignments.Top;
-
-				string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
-				string moddedBy = ModTranslation.GetString("moddedBy");
-				string continuedBy = ModTranslation.GetString("continuedBy", "Continued by {0}",
-					"<color=#FFFFE0>Among us</color> & <color=#00FFFF>HayashiUme</color>");
-				string ping = ModTranslation.GetString("ping", "Ping: {0} ms", AmongUsClient.Instance.Ping);
-
-				__instance.text.text = $"<color=#ff351f>CursedAmongUs</color> v{version}\n{moddedBy}\n{continuedBy}\n{ping}";
-				if (AmongUsClient.Instance.GameState != InnerNet.InnerNetClient.GameStates.Started)
+				if (_allowShowModInfos)
 				{
-					position.DistanceFromEdge = new Vector3(0f, 0.1f, 0);
+					__instance.text.alignment = TextAlignmentOptions.Top;
+					var position = __instance.GetComponent<AspectPosition>();
+					position.Alignment = AspectPosition.EdgeAlignments.Top;
+
+					string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+					string moddedBy = ModTranslation.GetString("moddedBy");
+					string continuedBy = ModTranslation.GetString("continuedBy", "Continued by {0}",
+						"<color=#FFFFE0>Among us</color> & <color=#00FFFF>HayashiUme</color>");
+					string ping = ModTranslation.GetString("ping", "Ping: {0} ms", AmongUsClient.Instance.Ping);
+
+					__instance.text.text = $"<color=#ff351f>CursedAmongUs</color> v{version}\n{moddedBy}\n{continuedBy}\n{ping}";
+					if (AmongUsClient.Instance.GameState == InnerNet.InnerNetClient.GameStates.Started)
+					{
+						position.DistanceFromEdge = new Vector3(2.25f, 0.11f, 0);
+					}
+					else
+					{
+						position.DistanceFromEdge = new Vector3(0f, 0.1f, 0);
+					}
+					position.AdjustPosition();
 				}
-				position.AdjustPosition();
 			}
 		}
-	}
-	[HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start))]
-	public static class LogoPatch
-	{
-		public static SpriteRenderer renderer;
-		private static PingTracker instance;
-		public static bool moddedMainMenu = false;
-		static void Postfix(PingTracker __instance)
+		[HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start))]
+		public static class LogoPatch
 		{
-			if (moddedMainMenu)
+			public static SpriteRenderer renderer;
+			private static PingTracker instance;
+			static void Postfix(PingTracker __instance)
 			{
-				var cauLogo = new GameObject("bannerLogo_CAU");
-				cauLogo.transform.localPosition = new Vector3(2.0491f, 0.8f, 5f);
-				cauLogo.transform.localScale = new Vector3(3.4527f, 2.8873f, 1f);
-				renderer = cauLogo.AddComponent<SpriteRenderer>();
-				renderer.sprite = PictureLoad.LoadSprite("CursedAmongUs.Resources.Banner.png", 300f);
+				if (_allowShowModInfos)
+				{
+					var cauLogo = new GameObject("bannerLogo_CAU");
+					cauLogo.transform.localPosition = new Vector3(2.0491f, 0.8f, 5f);
+					cauLogo.transform.localScale = new Vector3(3.4527f, 2.8873f, 1f);
+					renderer = cauLogo.AddComponent<SpriteRenderer>();
+					renderer.sprite = PictureLoad.LoadSprite("CursedAmongUs.Resources.Banner.png", 300f);
 
+					instance = __instance;
 
-				instance = __instance;
-
-
-				var credentialObject = new GameObject("credentialsCAU");
-				var credentials = credentialObject.AddComponent<TextMeshPro>();
-				credentials.SetText($"<color=#ff351f>CursedAmongUs</color> v{Assembly.GetExecutingAssembly().GetName().Version}\nModded by Devs-Us \nContinued by <color=#FFFFE0>Among us(XiezibanWrite)</color>");
-				credentials.alignment = TMPro.TextAlignmentOptions.Center;
-				credentials.fontSize *= 0.05f;
-				credentials.transform.localPosition = new Vector3(2.0036f, -1f, 5f);
+					var credentialObject = new GameObject("credentialsCAU");
+					var credentials = credentialObject.AddComponent<TextMeshPro>();
+					credentials.SetText($"<color=#ff351f>CursedAmongUs</color> v{Assembly.GetExecutingAssembly().GetName().Version}\n{ModTranslation.GetString("moddedby")} \n{ModTranslation.GetString("continuedBy", "Continued by {0}",
+						"<color=#FFFFE0>Among us</color> & <color=#00FFFF>HayashiUme</color>")}");
+					credentials.alignment = TMPro.TextAlignmentOptions.Center;
+					credentials.fontSize *= 0.05f;
+					credentials.transform.localPosition = new Vector3(2.0036f, -1f, 5f);
+				}
 			}
 		}
 	}
+	
 	public static class PictureLoad
 	{
 		public static Dictionary<string, Sprite> CachedSprites = new();
@@ -101,9 +109,9 @@ namespace CursedAmongUs.Source
 	{
 		public static void Postfix([HarmonyArgument(0)] PluginInfo pluginInfo, [HarmonyArgument(1)] Assembly pluginAssembly)
 		{
-			if(pluginInfo.Metadata.GUID == "com.sinai.unityexplorer" || pluginInfo.Metadata.GUID == "gg.reactor.api")
+			if(pluginInfo.Metadata.GUID != "com.sinai.unityexplorer" || pluginInfo.Metadata.GUID != "at.duikbo.regioninstall")
 			{
-				LogoPatch.moddedMainMenu = true;
+				CredentialsPatch._allowShowModInfos = false;
 			}
 		}
 	}
